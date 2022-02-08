3a6348cc53be6876a714800ca4b5fc84afd359df
iamthe-Wraith <allhailthewraith@protonmail.com>
2022-02-08 06:57:53 -0500

    fixes error getting authKey from headers in identify
    when socket makes use of the identify middleware, the method for getting
    the authKey is different. was throwing an error because the way we had
    implemented didnt exist on the socket request object.

--- src/middleware/identify.ts
+++ src/middleware/identify.ts
@@ -5,12 +5,17 @@ import * as UserModel from '../services/user';
 import { IRequest } from '../types/request';
 
 const identify = async (req: Request, _: Response, next: NextFunction) => {
-  const authKey = req.header('authKey');
+  let authKey = req.header?.('authKey');
+
+  if (!authKey && !!req.headers) {
+    authKey = req.headers.authKey;
+  }
+
   if (!authKey) return next();
   try {
     const uid = await Session.verifySession(authKey);
     if (uid) {
-      const user = await UserModel.getUserById(req, uid, true);
+      const user = await UserModel.getUserById(req, uid);
       (req as IRequest).requestor = (user as IUserDocument);
       (req as IRequest).authKey = authKey;
     }

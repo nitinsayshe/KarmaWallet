60e1e1ce65f6f865ab72cb419b4b201a0a76bbd5
iamthe-Wraith <allhailthewraith@protonmail.com>
2022-02-08 15:46:06 -0500

    adds getUsers to admin user controller

--- src/controllers/admin/users.ts
+++ src/controllers/admin/users.ts
@@ -1,6 +1,31 @@
+import aqp from 'api-query-params';
 import { IRequestHandler } from '../../types/request';
 import * as output from '../../services/output';
+import * as UserService from '../../services/user';
+import { asCustomError } from '../../lib/customError';
 
-export const getUsers: IRequestHandler = (req, res) => {
-  output.api(req, res, { message: 'you got the users' });
+export const getUsers: IRequestHandler = async (req, res) => {
+  try {
+    const reqQuery = aqp(req.query, { skipKey: 'page' });
+    const query = {
+      ...reqQuery,
+    };
+
+    const queryEntries = Object.entries(reqQuery.filter);
+
+    if (queryEntries.length) {
+      query.filter = {
+        $and: [...queryEntries.map(([key, value]) => ({ [key]: value }))],
+      };
+    }
+
+    const results = await UserService.getUsers(req, query);
+
+    output.api(req, res, {
+      ...results,
+      docs: results.docs.map(d => UserService.getSharableUser(d)),
+    });
+  } catch (err) {
+    output.error(req, res, asCustomError(err));
+  }
 };

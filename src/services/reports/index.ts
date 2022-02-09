289264e3a4913530ff2244bc80948b1f099ff027
iamthe-Wraith <allhailthewraith@protonmail.com>
2022-02-09 06:32:06 -0500

    adds correct users with cards count, total cards and trans

--- src/services/reports/index.ts
+++ src/services/reports/index.ts
@@ -1,18 +1,33 @@
 import { asCustomError } from '../../lib/customError';
 import { CardModel } from '../../models/card';
+import { TransactionModel } from '../../models/transaction';
 import { UserModel } from '../../models/user';
 import { IRequest } from '../../types/request';
 
 export const getSummary = async (_: IRequest) => {
   try {
     const totalUsersCount = await UserModel.find({}).count();
-    const usersWithCards = await CardModel.distinct('userId').count();
+    const cards = await CardModel.find({});
+    const transactions = await TransactionModel.find({}).count();
+    const usersWithCards = new Set();
+
+    for (const card of cards) {
+      if (!usersWithCards.has(card.userId.toString())) {
+        usersWithCards.add(card.userId.toString());
+      }
+    }
 
     return {
       users: {
         total: totalUsersCount,
-        withCard: usersWithCards,
-        withoutCards: totalUsersCount - usersWithCards,
+        withCard: usersWithCards.size,
+        withoutCard: totalUsersCount - usersWithCards.size,
+      },
+      cards: {
+        total: cards.length,
+      },
+      transactions: {
+        total: transactions,
       },
     };
   } catch (err) {

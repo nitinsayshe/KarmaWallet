59ab333ec84b4a4566d58bb8f24235fcf053b259
iamthe-Wraith <allhailthewraith@protonmail.com>
2022-02-10 11:06:15 -0500

    migrates impact routes, controller and service from backend

--- src/lib/number.ts
+++ src/lib/number.ts
@@ -1,1 +1,11 @@
+export const convertKgToMT = (num: number) => num * 0.001;
+
+export const formatNumber = new Intl.NumberFormat('en-us', { maximumSignificantDigits: 3 }).format;
+
 export const getRandom = (min: number, max: number) => Math.round(Math.random() * (max - min)) + min;
+
+export const getRandomInt = (min: number, max: number) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};

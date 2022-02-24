778a2e0ef0f5fede6002c9f580d6195abeb3e525
iamthe-Wraith <allhailthewraith@protonmail.com>
2022-02-24 06:26:29 -0500

    uncomments migration code

--- bin/reset_v3_migration.ts
+++ bin/reset_v3_migration.ts
@@ -1,32 +1,32 @@
 import 'dotenv/config';
 import { MongoClient } from '../src/clients/mongo';
-// import { reset as resetPlaidMapping } from '../src/integrations/plaid';
+import { reset as resetPlaidMapping } from '../src/integrations/plaid';
 import { asCustomError } from '../src/lib/customError';
 import { LegacyUserModel } from '../src/models/legacyUser';
 import { Logger } from '../src/services/logger';
-// import { resetCompanyMapping } from '../src/services/mappers/new_companies';
+import { resetCompanyMapping } from '../src/services/mappers/new_companies';
 import { resetSectors } from '../src/services/mappers/new_sectors';
-// import { IRequest } from '../src/types/request';
+import { IRequest } from '../src/types/request';
 
 (async () => {
   try {
-    // const mockRequest = ({
-    //   requestor: {},
-    //   authKey: '',
-    // } as IRequest);
+    const mockRequest = ({
+      requestor: {},
+      authKey: '',
+    } as IRequest);
     await MongoClient.init();
 
     console.log('\nresetting legacy users...');
     await LegacyUserModel.deleteMany({});
     console.log('[+] legacy users reset successfully');
 
-    // await resetCompanyMapping(mockRequest);
+    await resetCompanyMapping(mockRequest);
 
     await resetSectors();
 
-    // console.log('resetting plaid mapping...');
-    // await resetPlaidMapping(mockRequest);
-    // console.log('[+] plaid mapping reset successfully');
+    console.log('resetting plaid mapping...');
+    await resetPlaidMapping(mockRequest);
+    console.log('[+] plaid mapping reset successfully');
 
     await MongoClient.disconnect();
   } catch (err) {

306a3f54de8b1f08d71bda016122c048501cb72c
John D'Alessandro <jdealla@gmail.com>
2022-03-29 17:02:25 -0400

    Updated AWS SDK client

--- src/clients/aws.ts
+++ src/clients/aws.ts
@@ -1,33 +1,27 @@
-import aws, { SES, S3 } from 'aws-sdk';
-import { SdkClient } from './sdkClient';
-import { EmailAddresses } from '../lib/constants';
+import aws from 'aws-sdk';
 import { Logger } from '../services/logger';
-import { asCustomError } from '../lib/customError';
-
-aws.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: 'us-east-1',
-});
+import CustomError, { asCustomError } from '../lib/customError';
+import { SdkClient } from './sdkClient';
+import { EmailAddresses, ErrorTypes } from '../lib/constants';
 
 interface IAwsClient {
-  ses: SES;
-  s3: S3;
+  s3: aws.S3,
+  ses: aws.SES
 }
 
 interface ISendEmailRequest {
-  senderEmail?: SES.Address;
-  recipientEmail: SES.Address;
-  replyToAddresses: SES.AddressList;
+  senderEmail?: aws.SES.Address;
+  recipientEmail: aws.SES.Address;
+  replyToAddresses: aws.SES.AddressList;
   template: string;
   subject: string;
   senderName?: string;
 }
 
 interface IUploadRequest {
-  acl?: S3.ObjectCannedACL;
-  bucket?: S3.BucketName;
-  contentType?: S3.ContentType;
+  acl?: aws.S3.ObjectCannedACL;
+  bucket?: aws.S3.BucketName;
+  contentType?: aws.S3.ContentType;
   ext: string;
   file: Buffer;
   name: string;
@@ -37,19 +31,21 @@ export class AwsClient extends SdkClient {
   _client: IAwsClient = null;
 
   constructor() {
-    super('AWS');
+    super('Rare');
   }
 
-  _init = () => {
+  protected _init() {
     aws.config.update({
       accessKeyId: process.env.AWS_ACCESS_KEY_ID,
       secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      region: 'us-east-1',
     });
+
     this._client = {
       ses: new aws.SES(),
       s3: new aws.S3(),
     };
-  };
+  }
 
   sendMail = ({
     senderName = 'KarmaWallet',
@@ -59,7 +55,6 @@ export class AwsClient extends SdkClient {
     subject,
     replyToAddresses = [EmailAddresses.ReplyTo],
   }: ISendEmailRequest) => {
-    // https://betterprogramming.pub/how-to-send-emails-with-node-js-using-amazon-ses-8ae38f6312e4
     const params = {
       Source: `${senderName} <${senderEmail}>`,
       Destination: {
@@ -93,9 +88,9 @@ export class AwsClient extends SdkClient {
     name,
   }: IUploadRequest) => {
     try {
-      if (!file) return { error: 'An image file is required.', code: 422 };
-      if (!name) return { error: 'A filename is required.', code: 422 };
-      if (!ext) return { error: 'A file extension is required.', code: 422 };
+      if (!file) throw new CustomError('An image file is required.', ErrorTypes.INVALID_ARG);
+      if (!name) throw new CustomError('An file name is required.', ErrorTypes.INVALID_ARG);
+      if (!ext) throw new CustomError('An file extension is required.', ErrorTypes.INVALID_ARG);
 
       const imageData = {
         ACL: acl,

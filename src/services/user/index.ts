27db12cc6324f370b8d8a486208583bcf057bf87
John D'Alessandro <jdealla@gmail.com>
2022-04-27 10:10:35 -0400

    Updated user email to keep primary email on legacyuser

--- src/services/user/index.ts
+++ src/services/user/index.ts
@@ -239,13 +239,12 @@ export const updateUserEmail = async ({ user, legacyUser, email, req, pw }: IUpd
     user.emails = user.emails.map(userEmail => ({ email: userEmail.email, status: userEmail.status, primary: false }));
     user.emails.push({ email, status: UserEmailStatus.Unverified, primary: true });
     // TODO: remove when legacy user is removed
-    legacyUser.emails = legacyUser.emails.map(userEmail => ({ email: userEmail.email, status: userEmail.status, primary: false }));
-    legacyUser.emails.push({ email, status: UserEmailStatus.Unverified, primary: true });
+    legacyUser.emails = user.emails;
     // updating requestor for access to new email
     resendEmailVerification({ ...req, requestor: user });
   } else {
     user.emails = user.emails.map(userEmail => ({ email: userEmail.email, status: userEmail.status, primary: email === userEmail.email }));
-    legacyUser.emails = legacyUser.emails.map(userEmail => ({ email: userEmail.email, status: userEmail.status, primary: email === userEmail.email }));
+    legacyUser.emails = user.emails;
   }
 };
 

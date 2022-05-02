2ba87fc569e7a713a1322241cfe773fb1ff94aa6
iamthe-Wraith <allhailthewraith@protonmail.com>
2022-05-02 07:50:47 -0400

    extracts shareable functions into service/impact/utils

--- src/jobs/generateUserImpactTotals.ts
+++ src/jobs/generateUserImpactTotals.ts
@@ -2,11 +2,10 @@ import { count } from 'aws-sdk/clients/health';
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 import { Types } from 'mongoose';
-import { ICompanyDocument } from '../models/company';
-import { ITransactionDocument, TransactionModel } from '../models/transaction';
+import { TransactionModel } from '../models/transaction';
 import { IUserDocument, UserModel } from '../models/user';
 import { IUserImpactMonthData, IUserImpactSummary, IUserImpactTotalDocument, IUserImpactTotalScores, UserImpactTotalModel } from '../models/userImpactTotals';
-import { getUserImpactRatings } from '../services/impact/utils';
+import { getImpactScores, getImpactSummary, getMonthlyImpactBreakdown, getUserImpactRatings } from '../services/impact/utils';
 
 dayjs.extend(utc);
 
@@ -14,111 +13,6 @@ dayjs.extend(utc);
  * this file contains all the logic used to calculate
  * user_impact_totals.
  */
-
-interface IImpactSummary {
-  scores: { [key: string]: number };
-  total: number;
-}
-
-enum Ratings {
-  Positive = 'positive',
-  Neutral = 'neutral',
-  Negative = 'negative',
-}
-
-const getCompanyRating = ([neg, neut, pos]: [number, number][], score: number) => {
-  if (score === null) return null;
-
-  if (score >= neg[0] && score <= neg[1]) return Ratings.Negative;
-  if (score >= neut[0] && score <= neut[1]) return Ratings.Neutral;
-  if (score >= pos[0] && score <= pos[1]) return Ratings.Positive;
-
-  return null;
-};
-
-const getImpactScores = ({ scores, total }: IImpactSummary, ratings: [number, number][]) => {
-  const impactScores = { score: 0, positive: 0, negative: 0, neutral: 0 };
-
-  const _scores = Object.keys(scores)
-    .map(s => parseFloat(s));
-
-  for (const score of _scores) {
-    const amount = scores[`${score}`];
-    const totalPercentage = amount / total;
-    const rawImpact = totalPercentage * score;
-    const rating = getCompanyRating(ratings, score);
-    impactScores.score += rawImpact;
-    impactScores[rating] += totalPercentage;
-  }
-
-  return impactScores;
-};
-
-const getImpactSummary = (transactions: ITransactionDocument[]): IImpactSummary => {
-  const scores: { [key: string]: number } = {};
-  let total = 0;
-
-  for (const transaction of transactions) {
-    const { amount } = transaction;
-    const { combinedScore } = transaction.company as ICompanyDocument;
-
-    // exclude companies with a null combined score
-    if (typeof combinedScore !== 'number') continue;
-
-    const _combinedScore = `${combinedScore}`;
-
-    if (!scores[_combinedScore]) scores[_combinedScore] = 0;
-
-    scores[_combinedScore] += amount;
-    total += amount;
-  }
-
-  return {
-    scores,
-    total,
-  };
-};
-
-const getMonthStartDate = (date: dayjs.Dayjs) => date
-  .set('date', 1)
-  .set('hours', 0)
-  .set('minutes', 0)
-  .set('seconds', 0)
-  .set('milliseconds', 0);
-
-const getMonthlyImpactBreakdown = (transactions: ITransactionDocument[], ratings: [number, number][]) => {
-  let date = dayjs().utc();
-  const dateFormat = 'MM-YYYY';
-  const monthlyBreakdown: IUserImpactMonthData[] = [];
-  const allMonthlyTransactions: { month: dayjs.Dayjs, transactions: ITransactionDocument[] }[] = [];
-
-  for (const transaction of transactions) {
-    const transactionsDate = dayjs(transaction.date);
-    while (transactionsDate.format(dateFormat) !== date.format(dateFormat)) {
-      date = date.subtract(1, 'month');
-      allMonthlyTransactions.push({ month: getMonthStartDate(date), transactions: [] });
-    }
-
-    if (!allMonthlyTransactions[allMonthlyTransactions.length - 1]?.transactions) {
-      allMonthlyTransactions.push({ month: getMonthStartDate(date), transactions: [] });
-    }
-
-    allMonthlyTransactions[allMonthlyTransactions.length - 1].transactions.push(transaction);
-  }
-
-  for (const monthlyTransactions of allMonthlyTransactions) {
-    const summary = getImpactSummary(monthlyTransactions.transactions);
-    const impactScores = getImpactScores(summary, ratings);
-
-    monthlyBreakdown.push({
-      ...impactScores,
-      date: monthlyTransactions.month.toDate(),
-      transactionCount: monthlyTransactions.transactions.length,
-    });
-  }
-
-  return monthlyBreakdown;
-};
 
 const saveUserImpactTotal = async (
   user: IUserDocument,

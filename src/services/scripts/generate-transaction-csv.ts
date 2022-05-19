ef856eef626ba651a76eb649de12f503887e0f6b
iamthe-Wraith <allhailthewraith@protonmail.com>
2022-05-19 09:39:28 -0400

    updates transaction query with dif populate model to avoid multi db hits

--- src/services/scripts/generate-transaction-csv.ts
+++ src/services/scripts/generate-transaction-csv.ts
@@ -7,14 +7,13 @@ import { CompanyModel } from '../../models/company';
 export const generateTransactionCsv = async () => {
   console.log('\ngenerating transaction csv...');
   try {
-    // since loadModels was removed from mongo client we need to import company model to be able to populate
-    await CompanyModel.findOne({ legacyId: 10000 });
     const transactions = await TransactionModel.find({})
       .populate(
         [
           {
+            // TODO: update this to `company` once Tongass is launched
             path: 'companyId',
-            model: 'company',
+            model: CompanyModel,
           },
         ],
       ).lean();
@@ -22,7 +21,7 @@ export const generateTransactionCsv = async () => {
     const _csv = parse(transactions);
     fs.writeFileSync(path.join(__dirname, '.tmp', 'transactions.csv'), _csv);
 
-    console.log(`[+] transaction repcsvort generated successfully with ${transactions.length} transactions\n`);
+    console.log(`[+] transaction report generated successfully with ${transactions.length} transactions\n`);
   } catch (err: any) {
     console.log('[-] error generating transaction csv');
     console.log(err.message);

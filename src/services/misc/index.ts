4f9b403f34779fe52bde43e0693618f3543e71d1
John DAlessandro <jdealla@gmail.com>
2022-05-24 08:40:22 -0400

    Added error handling for companyRatingsThresholds

--- src/services/misc/index.ts
+++ src/services/misc/index.ts
@@ -1,6 +1,9 @@
+import { ErrorTypes } from '../../lib/constants';
+import CustomError from '../../lib/customError';
 import { MiscModel } from '../../models/misc';
 
 export const getCompanyRatingsThresholds = async () => {
   const _companyRatingsThresholds = await MiscModel.findOne({ key: 'company-ratings-thresholds' });
+  if (!_companyRatingsThresholds || !_companyRatingsThresholds?.value) throw new CustomError('Company ratings thresholds not found', ErrorTypes.NOT_FOUND);
   return JSON.parse(_companyRatingsThresholds?.value);
 };

ee9062aeca3883d865a2e37609145f0783e410a0
iamthe-Wraith <allhailthewraith@protonmail.com>
2022-06-05 00:12:12 -0400

    updates bin/scripts,ts with all tongass release scripts

--- bin/scripts.ts
+++ bin/scripts.ts
@@ -3,7 +3,14 @@ import 'dotenv/config';
 import { MongoClient } from '../src/clients/mongo';
 import { asCustomError } from '../src/lib/customError';
 import { Logger } from '../src/services/logger';
-import { generateTransactionCsv } from '../src/services/scripts/generate-transaction-csv';
+import { calculateAllCompanyScores } from '../src/services/scripts/calculate_company_scores';
+import { cleanTransactions } from '../src/services/scripts/clean_transactions';
+import { createDataSources } from '../src/services/scripts/create_data_sources';
+import { mapCompanies2DataSources } from '../src/services/scripts/map_companies_2_data_sources';
+import { mapDataSourcesToUNSDGs } from '../src/services/scripts/map_data_sources_to_unsdgs';
+import { mapSectorsToTransactions } from '../src/services/scripts/map_sectors_to_transactions';
+import { mapUNSDGs } from '../src/services/scripts/new_unsdgs';
+import { updateCompanySectorsWithPrimaryStatus } from '../src/services/scripts/update-sectors-with-primary';
 
 (async () => {
   try {
@@ -13,16 +20,18 @@ import { generateTransactionCsv } from '../src/services/scripts/generate-transac
     // } as IRequest);
     await MongoClient.init();
 
-    await generateTransactionCsv();
-
     // add mappers here...
-    // await createDataSources();
-    // await mapDataSourcesToUNSDGs();
-    // await mapCompanies2DataSources();
-    // await calculateAllCompanyScores();
-    // await generateCompanyDataSourceMappingReport();
 
-    // add mappers here...
+    // await mapLegacyUserPwToNewUserPw();
+    await mapUNSDGs();
+    await updateCompanySectorsWithPrimaryStatus();
+    await cleanTransactions();
+    await mapSectorsToTransactions();
+    await createDataSources();
+    await mapDataSourcesToUNSDGs();
+    await mapCompanies2DataSources();
+    await calculateAllCompanyScores();
+
     await MongoClient.disconnect();
   } catch (err) {
     console.log('\n[-] something went wrong during the migration!');

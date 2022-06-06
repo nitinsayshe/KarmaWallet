ac2b9113880a94b19ab2ce26bf9a53e94c26a96a
Jake Lundberg <allhailthewraith@protonmail.com>
2022-06-06 15:18:41 -0400

    updates list of sectors to exclude for getTopSectors endpoint (#214)

--- bin/scripts.ts
+++ bin/scripts.ts
@@ -3,14 +3,7 @@ import 'dotenv/config';
 import { MongoClient } from '../src/clients/mongo';
 import { asCustomError } from '../src/lib/customError';
 import { Logger } from '../src/services/logger';
-import { calculateAllCompanyScores } from '../src/services/scripts/calculate_company_scores';
-import { cleanTransactions } from '../src/services/scripts/clean_transactions';
-import { createDataSources } from '../src/services/scripts/create_data_sources';
-import { mapCompanies2DataSources } from '../src/services/scripts/map_companies_2_data_sources';
-import { mapDataSourcesToUNSDGs } from '../src/services/scripts/map_data_sources_to_unsdgs';
-import { mapSectorsToTransactions } from '../src/services/scripts/map_sectors_to_transactions';
-import { mapUNSDGs } from '../src/services/scripts/new_unsdgs';
-import { updateCompanySectorsWithPrimaryStatus } from '../src/services/scripts/update-sectors-with-primary';
+import { reportPosCompaniesInSectors } from '../src/services/scripts/pos_companies';
 
 (async () => {
   try {
@@ -23,14 +16,16 @@ import { updateCompanySectorsWithPrimaryStatus } from '../src/services/scripts/u
     // add mappers here...
 
     // await mapLegacyUserPwToNewUserPw();
-    await mapUNSDGs();
-    await updateCompanySectorsWithPrimaryStatus();
-    await cleanTransactions();
-    await mapSectorsToTransactions();
-    await createDataSources();
-    await mapDataSourcesToUNSDGs();
-    await mapCompanies2DataSources();
-    await calculateAllCompanyScores();
+    // await mapUNSDGs();
+    // await updateCompanySectorsWithPrimaryStatus();
+    // await cleanTransactions();
+    // await mapSectorsToTransactions();
+    // await createDataSources();
+    // await mapDataSourcesToUNSDGs();
+    // await mapCompanies2DataSources();
+    // await calculateAllCompanyScores();
+
+    await reportPosCompaniesInSectors();
 
     await MongoClient.disconnect();
   } catch (err) {

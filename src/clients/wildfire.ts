edebc5be9beb67e96f90757bae3377b2579693e3
John DAlessandro <jdealla@gmail.com>
2022-09-07 10:49:58 -0400

    Updated wildfire client

--- src/clients/wildfire.ts
+++ src/clients/wildfire.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
+import CustomError, { asCustomError } from '../lib/customError';
 import { SdkClient } from './sdkClient';
 
 dayjs.extend(utc);
@@ -27,11 +28,82 @@ export class WildfireClient extends SdkClient {
   protected _init() {
     this._client = axios.create({
       headers: {
-        'x-api-key': WILDFIRE_CLIENT_APP_KEY,
         'Content-Type': 'application/json',
         Accept: 'application/json',
       },
-      baseURL: `https://${process.env.RARE_ENV}.catch-carbon-api.rare.org`,
+      baseURL: `https://www.wildlink.me/data/${WILDFIRE_CLIENT_APP_ID}`,
     });
   }
+
+  getMerchantRates = async () => {
+    let data;
+    try {
+      data = await this._client.get('/merchant-rate/1');
+    } catch (err) {
+      console.log(err);
+      throw asCustomError(err);
+    }
+    if (!data) throw new CustomError('No merchant rate data returned from Wildfire');
+    return data;
+  };
+
+  getCoupons = async () => {
+    let data;
+    try {
+      data = await this._client.get('/coupon/1');
+    } catch (err) {
+      console.log(err);
+      throw asCustomError(err);
+    }
+    if (!data) throw new CustomError('No coupon data returned from Wildfire');
+    return data;
+  };
+
+  getActiveDomains = async () => {
+    let data;
+    try {
+      data = await this._client.get('/active-domain/1');
+    } catch (err) {
+      console.log(err);
+      throw asCustomError(err);
+    }
+    if (!data) throw new CustomError('No active-domain data returned from Wildfire');
+    return data;
+  };
+
+  getMerchants = async () => {
+    let data;
+    try {
+      data = await this._client.get('/merchant/1');
+    } catch (err) {
+      console.log(err);
+      throw asCustomError(err);
+    }
+    if (!data) throw new CustomError('No merchant data returned from Wildfire');
+    return data;
+  };
+
+  getCategoryData = async () => {
+    let data;
+    try {
+      data = await this._client.get('/category/1');
+    } catch (err) {
+      console.log(err);
+      throw asCustomError(err);
+    }
+    if (!data) throw new CustomError('No category data returned from Wildfire');
+    return data;
+  };
+
+  getFeaturedMerchantData = async () => {
+    let data;
+    try {
+      data = await this._client.get('/featured-merchant/1');
+    } catch (err) {
+      console.log(err);
+      throw asCustomError(err);
+    }
+    if (!data) throw new CustomError('No featured merchant data returned from Wildfire');
+    return data;
+  };
 }

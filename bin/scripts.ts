4df82842b2363f1ba740c26a90bbf3e5643072da
John DAlessandro <jdealla@gmail.com>
2022-09-22 14:11:25 -0400

    Updated commission allocation logic

--- bin/scripts.ts
+++ bin/scripts.ts
@@ -1,10 +1,14 @@
+/* eslint-disable unused-imports/no-unused-imports */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable camelcase */
+/* eslint-disable no-unused-vars */
 import 'dotenv/config';
 import { asCustomError } from '../src/lib/customError';
 import { Logger } from '../src/services/logger';
 import { WildfireClient } from '../src/clients/wildfire';
 import { mapWildfireCommissionToKarmaCommission } from '../src/services/commission/utils';
 import { MongoClient } from '../src/clients/mongo';
+import { CommissionModel } from '../src/models/commissions';
 
 (async () => {
   try {
@@ -19,13 +23,21 @@ import { MongoClient } from '../src/clients/mongo';
     // await GenerateGroupStatements.exec();
     // await removeMerchant('63079ac5e33a266250fb7ce4');
     // await removeDuplicateWildfireMerchants();
-    const wildfireClient = new WildfireClient();
-    const res = await wildfireClient.getAdminComissionDetails({ startDate: '2022-07-01', endDate: '2022-09-15' });
-
-    for (const commission of res.data.Commissions) {
-      await mapWildfireCommissionToKarmaCommission({ ...commission, TC: '62f6761cf5e3ffdae60ef249' });
+    const commissions = await CommissionModel.find({});
+    for (const commission of commissions) {
+      console.log(`\nUpdating commission ${commission._id}`);
+      const { amount, allocation } = commission;
+      console.log({ amount, user: allocation.user, karma: allocation.karma });
+      const newUserAllocation = Math.floor((amount * 0.75) * 100) / 100;
+      const newKarmaAllocation = amount - newUserAllocation;
+      console.log({ newUserAllocation, newKarmaAllocation });
+      const totalAllocation = newUserAllocation + newKarmaAllocation;
+      console.log('total allocation', totalAllocation);
+      console.log('is equal or less than amount?', totalAllocation <= amount);
+      commission.allocation.user = newUserAllocation;
+      commission.allocation.karma = newKarmaAllocation;
+      await commission.save();
     }
-
     await MongoClient.disconnect();
   } catch (err) {
     Logger.error(asCustomError(err));

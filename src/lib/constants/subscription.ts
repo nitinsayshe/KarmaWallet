6ece3bcfa5c1167517ef64484d35a4452162bf63
adelgado <adelgado0723@gmail.com>
2022-12-02 09:15:50 -0500

    fixes typo in eployer benefit subscription code

--- src/lib/constants/subscription.ts
+++ src/lib/constants/subscription.ts
@@ -11,14 +11,14 @@ export const SubscriptionCodeToProviderProductId = {
   [SubscriptionCode.accountUpdates]: ActiveCampaignListId.AccountUpdates,
   [SubscriptionCode.monthlyNewsletters]: ActiveCampaignListId.MonthyNewsletters,
   [SubscriptionCode.generalUpdates]: ActiveCampaignListId.GeneralUpdates,
-  [SubscriptionCode.groupInterestsEmplpyerBenefit]: HubspotFormId.groupsInterest,
+  [SubscriptionCode.groupInterestsEmployerBenefit]: HubspotFormId.groupsInterest,
   [SubscriptionCode.groupInterestsNonProfit]: HubspotFormId.groupsInterest,
   [SubscriptionCode.groupInterestsSocialMediaCommunity]: HubspotFormId.groupsInterest,
   [SubscriptionCode.groupInterestsOther]: HubspotFormId.groupsInterest,
 };
 
 export const InterestCategoryToSubscriptionCode = {
-  [InterestCategory.EmployerBenefit]: SubscriptionCode.groupInterestsEmplpyerBenefit,
+  [InterestCategory.EmployerBenefit]: SubscriptionCode.groupInterestsEmployerBenefit,
   [InterestCategory.NonProfit]: SubscriptionCode.groupInterestsNonProfit,
   [InterestCategory.SocialMediaCommunity]: SubscriptionCode.groupInterestsSocialMediaCommunity,
   [InterestCategory.Other]: SubscriptionCode.groupInterestsOther,

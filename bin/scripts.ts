5de28a0cb8c72f7b9e03cc70e94e6253fc517648
Andres Delgado <adelgado0723@gmail.com>
2023-03-24 16:14:19 -0400

    Active Campaign: Debugging Refactor  (#392)
    * working on setting Carbon Offset to 0 for users
    
    * creates script for testing
    
    * removes code in script file
    
    ---------
    
    Co-authored-by: Sara Wall <smwall84@gmail.com>

--- bin/scripts.ts
+++ bin/scripts.ts
@@ -2,27 +2,22 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable camelcase */
 /* eslint-disable no-unused-vars */
-import 'dotenv/config';
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
+import 'dotenv/config';
 import { MongoClient } from '../src/clients/mongo';
 import { asCustomError } from '../src/lib/customError';
 import { Logger } from '../src/services/logger';
-import { UserGroupModel } from '../src/models/userGroup';
 
 dayjs.extend(utc);
 
 (async () => {
   try {
-    // const mockRequest = ({
-    //   requestor: { },
-    //   authKey: '',
-    // } as IRequest);
     await MongoClient.init();
-    await MongoClient.disconnect();
   } catch (err) {
     Logger.error(asCustomError(err));
     console.log(err);
-    // await MongoClient.disconnect();
+  } finally {
+    MongoClient.disconnect();
   }
 })();

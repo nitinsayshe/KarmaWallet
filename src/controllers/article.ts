280a4abce8910c7a4b63dc1439c45fec17988239
senadzenkovic <zenkovic88@gmail.com>
2023-04-17 17:18:39 -0400

    updates endpoint to work with real data from staging

--- src/controllers/article.ts
+++ src/controllers/article.ts
@@ -5,13 +5,11 @@ import * as ArticleService from '../services/article';
 
 export const getArticleById: IRequestHandler<{ articleId: string }> = async (req, res) => {
   try {
-    console.log('Hittt///');
     const { articleId } = req.params;
     const article = await ArticleService.getArticleById(req, articleId);
     console.log(article);
     api(req, res, article);
   } catch (err) {
-    console.log('Hittt///');
     error(req, res, asCustomError(err));
   }
 };

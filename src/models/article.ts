b00a0bc7a596e9d49735fc47daae852f495d13db
senadzenkovic <zenkovic88@gmail.com>
2023-04-17 14:33:55 -0400

    adds article schema

---
+++ src/models/article.ts
@@ -0,0 +1,61 @@
+import {
+  Schema,
+  model,
+  Document,
+  Model,
+  ObjectId,
+} from 'mongoose';
+import { IModel } from '../types/model';
+
+export enum ImageAlignment {
+  Left = 'Left',
+  Right = 'Right',
+  Above = 'Above'
+}
+export interface ITheBad {
+  paragraphTitle: string;
+  paragraphBody: string;
+  imageUrl?: string;
+  imageSource?: string;
+  imageAlignment?: ImageAlignment;
+}
+
+export interface ITheGood extends ITheBad {
+  quote: string;
+  initiatives: [string];
+}
+
+export interface IArticle {
+  _id: ObjectId;
+  companyId: string;
+  dateWritten: string;
+  bannerImageUrl: string;
+  introParagraph: string;
+  theGood: ITheGood[];
+  theBad: ITheBad[];
+}
+
+export interface IArticleDocument extends IArticle, Document {
+  _id: ObjectId;
+}
+
+export type IArticleModel = IModel<IArticle>;
+
+const articleSchema = new Schema({
+  companyId: { type: String, required: true },
+  dateWritten: { type: String, required: true },
+  bannerImageUrl: { type: String, required: true },
+  introParagraph: { type: String, required: true },
+  theGood: [{
+    type: {
+      paragraphTitle: { type: String },
+      paragraphBody: { type: String },
+      imageUrl: { type: String },
+      imageSource: { type: String },
+      imageAlingment: { type: String, enum: Object.values(ImageAlignment) },
+    },
+    required: true,
+  }],
+});
+
+export const ArticleModel = model<IArticleDocument, Model<IArticle>>('article', articleSchema);

b81823cf4e5af48b50ea5c8a72d5274cc7916d0c
Sara Wall <smwall84@gmail.com>
2023-04-26 14:56:27 -0400

    adds one time send

--- src/jobs/sendCreateAccountReminderEmail.ts
+++ src/jobs/sendCreateAccountReminderEmail.ts
@@ -13,6 +13,7 @@ export const exec = async () => {
     const query: FilterQuery<IVisitorDocument> = {
       user: null,
       emailStatus: { $ne: null },
+      createdOn: { $lte: dayjs().subtract(15, 'days').endOf('day').toDate() },
       $or: [
         {
           $and: [
@@ -27,6 +28,39 @@ export const exec = async () => {
           ],
         },
       ],
+    };
+
+    const visitors = await VisitorModel.find(query);
+
+    if (!visitors || !visitors.length) throw new CustomError('No visitors to nudge.', ErrorTypes.GEN);
+
+    for (const visitor of visitors) {
+      const days = emailVerificationDays;
+      const { email } = visitor;
+      if (!isemail.validate(email, { minDomainAtoms: 2 })) {
+        console.log(`[+] Invalid email format for visitor ${visitor._id}: ${email}`);
+        continue;
+      }
+
+      const token = await createVisitorToken({ visitor, days, type: TokenTypes.Email, resource: { email } });
+      const reminderEmail = await sendAccountCreationReminderEmail({ token: token.value, recipientEmail: email, name: 'createAccountEmailReminder', visitor });
+      if (!!reminderEmail) emailsSent += 1;
+      console.log(`[+] Nudge email sent to visitor ${emailsSent} visitors`);
+    }
+    console.log(`[+] Nudge emails sent to ${emailsSent} visitors}`);
+  } catch (err) {
+    console.log('Error nudging visitors to finish account creation', err);
+    throw err;
+  }
+};
+
+export const oneTimeSend = async () => {
+  let emailsSent = 0;
+  try {
+    const query: FilterQuery<IVisitorDocument> = {
+      user: null,
+      emailStatus: { $ne: null },
+      createdOn: { $lte: dayjs().subtract(15, 'days').endOf('day').toDate() },
     };
 
     const visitors = await VisitorModel.find(query);

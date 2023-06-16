c7555c3f0dca14642cc147624f344f8f342719f3
John DAlessandro <jdealla@gmail.com>
2023-06-16 13:00:16 -0400

    updated for html validation

--- src/services/utilities/index.ts
+++ src/services/utilities/index.ts
@@ -7,18 +7,32 @@ export interface IValidateHtmlBody {
   html: string;
 }
 
+export interface IValidateHtmlResult {
+  isValid: boolean;
+  errors: string[];
+}
+
 export const validateHtml = async (req: IRequest<{}, {}, IValidateHtmlBody>) => {
   const { html } = req.body;
   if (!html) throw new CustomError('HTML is required', ErrorTypes.INVALID_ARG);
+
+  const result: Partial<IValidateHtmlResult> = {
+    isValid: false,
+  };
+
   const options: validator.OptionsForHtmlFileAsValidationTargetAndObjectAsResult = {
     data: html,
     isFragment: true,
     // messages that are coming back that should be ignored need to be added verbatim here
-    ignore: [
-      'Trailing slash on void elements has no effect and interacts badly with unquoted attribute values.',
-    ],
   };
-  const result = await validator(options);
+  const validateResult = await validator(options);
+
+  result.isValid = typeof validateResult === 'string' && validateResult === 'The document validates according to the specified schema(s).';
+
+  console.log('\n\n\n\n', { validateResult }, '\n\n\n\n');
+
+  result.errors = [];
+
   console.log('\n\n -- ', result, ' -- \n\n');
-  return { result };
+  return result;
 };

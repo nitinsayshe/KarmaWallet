4e1e95fc0ad0e6c6604339b96677dd4624d9501b
nitinb-iprogrammer <nitinbs@iprogrammer.co>
2023-07-20 19:09:16 +0530

    update the routes for biometric register/remove

--- src/routers/integrations/index.ts
+++ src/routers/integrations/index.ts
@@ -8,7 +8,6 @@ import biometricRouter from './biometric';
  * this file is used for direct communication with the integration APIs
  * it should only be used when the response from the integration API should be served to the client
  * rather than it being used for side effects/state changes in the DB
- *
  * e.g.: the client needs a link token from Plaid and the link token is not stored in the DB or used for anything else other than to return to the client
  */
 

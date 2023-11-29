a6e8e2da4e6b288a9eb90e3fcb2b0109e2ac5632
Sara Wall <smwall84@gmail.com>
2023-11-29 08:06:41 -0500

    updates the firebase variables to not use secrets unless a secret is necessary

--- src/integrations/firebaseCloudMessaging/index.ts
+++ src/integrations/firebaseCloudMessaging/index.ts
@@ -4,17 +4,17 @@ import { IUserDocument } from '../../models/user';
 import { IFCMNotification, IPushNotification } from './types';
 
 export const serviceAccount = {
-  type: process.env.FIREBASE_TYPE,
+  type: 'service_account',
   project_id: process.env.FIREBASE_PROJECT_ID,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
   private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
-  token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
   client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+  universe_domain: 'googleapis.com',
 };
 
 // Initialize the Firebase Admin SDK (should be done before sending messages)

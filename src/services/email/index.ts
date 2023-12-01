052401c04e2acbee70d9e712b88967cfd96e4401
Sara Wall <smwall84@gmail.com>
2023-12-01 13:55:07 -0500

    updates text on welcome email

--- src/services/email/index.ts
+++ src/services/email/index.ts
@@ -476,7 +476,7 @@ export const sendKarmaCardWelcomeEmail = async ({
   const { isValid, missingFields } = verifyRequiredFields(['domain', 'recipientEmail', 'name', 'newUser'], { domain, recipientEmail, name, newUser });
   if (!isValid) throw new CustomError(`Fields ${missingFields.join(', ')} are required`, ErrorTypes.INVALID_ARG);
   const template = buildTemplate({ templateName: emailTemplateConfig.name, data: { name, domain, newUser } });
-  const subject = 'Welcome to the Karma Wallet Family!';
+  const subject = 'Welcome to Karma Wallet!';
   const jobData: IEmailJobData = { template, subject, senderEmail, recipientEmail, replyToAddresses, emailTemplateConfig, user };
   if (sendEmail) EmailBullClient.createJob(JobNames.SendEmail, jobData, defaultEmailJobOptions);
   return { jobData, jobOptions: defaultEmailJobOptions };

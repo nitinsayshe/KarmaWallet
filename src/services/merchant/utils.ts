1f65f027a02e064121c66f3f95a0947dda156192
Andy Delgado <adelgado0723@gmail.com>
2024-01-08 18:29:39 -0500

    refactors

--- src/services/merchant/utils.ts
+++ src/services/merchant/utils.ts
@@ -4,25 +4,11 @@ export const getMerchantRateDescription = (
   Kind: string | CommissionType,
   Amount: number,
 ): { maxDescription: string; maxAmount: string; amount: number } => {
-  let maxDescription = '';
-  if (!Kind) {
-    return {
-      maxAmount: Amount.toString(),
-      maxDescription,
-      amount: Amount,
-    };
-  }
+  const kind = Kind.toLowerCase();
+  const maxDescription = kind === 'percentage' || kind === 'percent'
+    ? `Up to ${Amount}%`
+    : `Up to ${Amount}`;
 
-  const kind = Kind?.toLowerCase();
-  if (kind !== 'fixed' && kind !== 'percentage' && kind !== 'flat') {
-    return {
-      maxDescription,
-      maxAmount: Amount.toString(),
-      amount: Amount,
-    };
-  }
-
-  maxDescription = `Up to ${Amount}`;
   return {
     maxDescription,
     maxAmount: Amount.toString(),

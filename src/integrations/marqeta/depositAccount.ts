0b5f053eeddf930b85d0804793f84484e9829b1e
nitinsayshe <nitinsayshe@gmail.com>
2024-03-14 19:26:27 +0530

    deposit-acccount-creation-apis

--- src/integrations/marqeta/depositAccount.ts
+++ src/integrations/marqeta/depositAccount.ts
@@ -2,6 +2,8 @@ import { DepositAccount } from '../../clients/marqeta/depositAccount';
 import { MarqetaClient } from '../../clients/marqeta/marqetaClient';
 import { IRequest } from '../../types/request';
 import { IMarqetaDepositAccount } from './types';
+import CustomError from '../../lib/customError';
+import { ErrorTypes } from '../../lib/constants';
 
 // Instantiate the MarqetaClient
 const marqetaClient = new MarqetaClient();
@@ -11,6 +13,9 @@ const depositAccount = new DepositAccount(marqetaClient);
 
 export const createDepositAccount = async (req: IRequest<{}, {}, IMarqetaDepositAccount>) => {
   const { userToken } = req.requestor.integrations.marqeta;
+  if (!userToken) {
+    throw new CustomError('userToken for marqeta user not found', ErrorTypes.NOT_FOUND);
+  }
   const params = { userToken, ...req.body };
   const data = await depositAccount.createDepositAccount(params);
   return { data };

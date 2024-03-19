0b31edd66c3a887452786734c8496e7ebe853c6a
Sara Wall <smwall84@gmail.com>
2024-03-19 14:53:35 -0400

    adds depositAccount info to routers

--- src/routers/depositAccount.ts
+++ src/routers/depositAccount.ts
@@ -4,10 +4,10 @@ import * as DepositAccountContoller from '../controllers/depositAccount';
 
 const router = Router();
 
-router.route('/')
-  .get(authenticate, DepositAccountContoller.getDepositAccounts);
-
 router.route('/active')
   .get(authenticate, DepositAccountContoller.getActiveDepositAccount);
+
+router.route('/')
+  .get(authenticate, DepositAccountContoller.getDepositAccounts);
 
 export default (app: Express) => app.use('/deposit-account', router);

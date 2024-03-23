77b653414ad3fb369aa67b4fe7c3445278eed940
nitinsayshe <nitinsayshe@gmail.com>
2024-03-23 22:17:45 +0530

    apple wallet provesioning

--- src/controllers/integrations/marqeta/digitalWalletManagement.ts
+++ src/controllers/integrations/marqeta/digitalWalletManagement.ts
@@ -28,7 +28,7 @@ export const googleWalletProvision: IRequestHandler<{}, {}, IGoogleWalletProvesi
     if (!isValid) {
       throw new Error(`Invalid input. Body requires the following fields: ${missingFields.join(', ')}.`);
     }
-    const { data } = await DigitalWalletManagementService.googleWalletProvision(req);
+    const data = await DigitalWalletManagementService.googleWalletProvision(req);
     output.api(req, res, data);
   } catch (err) {
     output.error(req, res, asCustomError(err));
@@ -43,7 +43,7 @@ export const samsungWalletProvision: IRequestHandler<{}, {}, ISamsungWalletProve
     if (!isValid) {
       throw new Error(`Invalid input. Body requires the following fields: ${missingFields.join(', ')}.`);
     }
-    const { data } = await DigitalWalletManagementService.samsungWalletProvision(req);
+    const data = await DigitalWalletManagementService.samsungWalletProvision(req);
     output.api(req, res, data);
   } catch (err) {
     output.error(req, res, asCustomError(err));
@@ -53,7 +53,7 @@ export const samsungWalletProvision: IRequestHandler<{}, {}, ISamsungWalletProve
 export const listDigitalWalletForUserCard: IRequestHandler<{ cardToken: string }, {}, {}> = async (req, res) => {
   try {
     const { cardToken } = req.params;
-    const { data } = await DigitalWalletManagementService.listDigitalWalletForUserCard(cardToken);
+    const data = await DigitalWalletManagementService.listDigitalWalletForUserCard(cardToken);
     output.api(req, res, data);
   } catch (err) {
     output.error(req, res, asCustomError(err));

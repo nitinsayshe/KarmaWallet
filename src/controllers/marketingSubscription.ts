e3741983fae8ac2bdbf6f5974134b667938cb7ae
Sara Wall <smwall84@gmail.com>
2024-05-13 14:50:45 -0400

    updates the router

--- src/controllers/marketingSubscription.ts
+++ src/controllers/marketingSubscription.ts
@@ -1,13 +1,13 @@
 import { asCustomError } from '../lib/customError';
 import { IRequestHandler } from '../types/request';
 import * as output from '../services/output';
-import * as SubscriptionService from '../services/marketing_subscription';
+import * as MarketingSubscriptionService from '../services/marketing_subscription';
 
-export const newsletterUnsubscribe: IRequestHandler<{}, {}, SubscriptionService.INewsletterUnsubscribeData> = async (req, res) => {
+export const newsletterUnsubscribe: IRequestHandler<{}, {}, MarketingSubscriptionService.INewsletterUnsubscribeData> = async (req, res) => {
   try {
     const { email, preserveSubscriptions } = req.body;
     if (process.env.NODE_ENV !== 'production') return output.api(req, res, null);
-    await SubscriptionService.newsletterUnsubscribe(req, email, preserveSubscriptions);
+    await MarketingSubscriptionService.newsletterUnsubscribe(req, email, preserveSubscriptions);
     output.api(req, res, null);
   } catch (err) {
     output.error(req, res, asCustomError(err));

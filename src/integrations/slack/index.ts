a5ef71989c1274fb4ac0ec70500e0772de418f61
Andy Delgado <adelgado0723@gmail.com>
2024-07-10 16:27:58 -0400

    adds logic for choosing the correct channel based on the provided environment and for sending messages

--- src/integrations/slack/index.ts
+++ src/integrations/slack/index.ts
@@ -1,6 +1,29 @@
 import { SlackClient } from '../../clients/slack';
+import { SlackChannelNameEnum } from '../../lib/constants';
 
 export const getSlackChannelId = async (channelName: string) => {
   const channels = await SlackClient.conversations.list();
   return channels.channels.find((channel) => channel.name === channelName)?.id;
+};
+
+export const getTransactionAlertChannelIdFromEnvironment = async () => {
+  if (process.env.NODE_ENV === 'production') {
+    return getSlackChannelId(SlackChannelNameEnum.ProdTransactionAlerts);
+  }
+  if (process.env.NODE_ENV === 'staging') {
+    return getSlackChannelId(SlackChannelNameEnum.StagingTransactionAlerts);
+  }
+  console.log(`Not logging transaction alert in this environment: ${process.env.NODE_ENV}`);
+  return null;
+};
+
+export const sendTransactionAlertMessage = async (message: string) => {
+  const channelId = await getTransactionAlertChannelIdFromEnvironment();
+  if (!channelId) {
+    return;
+  }
+  await SlackClient.chat.postMessage({
+    channel: channelId,
+    text: message,
+  });
 };
